ff0fa2cb6ed9c6adf6384e35f2465329c7089dbb
Anirudh S. Kumar <anirudh.skumar.03@gmail.com>
2023-03-21 02:11:10 +0530

    completed product page, doing checkout now

---
+++ src/lib/shared/buttons/DefaultButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let name: string = 'Button';
+    export let color:string = 'teal';
+
+    const colorVariants = {
+    'teal': 'flex ml-auto text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-teal-400 rounded',
+    'teal_outline': 'flex ml-auto text-teal-500 outline outline-1 py-2 px-6 focus:outline-2 rounded hover:text-teal-400',
+  }
+
+  let currentColor: string = '';
+
+  switch(color){
+    case 'teal':
+      currentColor = colorVariants.teal;
+      break;
+    case 'teal_outline':
+      currentColor = colorVariants.teal_outline;
+      break;
+    default:
+      currentColor = colorVariants.teal;
+      break;
+  }
+
+</script>
+
+<button class={currentColor}>
+    {name}
+</button>
+
